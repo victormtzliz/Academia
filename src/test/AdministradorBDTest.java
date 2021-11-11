ac3b5d38f43ad2f07b0c00e9a6390e38f3af5c2f
victormtzliz <victormtzliz@opendeusto.es>
2021-11-11 12:05:57 +0100

    estructura de los test

---
+++ src/test/AdministradorBDTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class AdministradorBDTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testCrearTablaAdministrador() {
+		fail("Not yet implemented");
+	}
+
+	public void testInsertarAdministrador() {
+		fail("Not yet implemented");
+	}
+
+	public void testTodosLosAdmins() {
+		fail("Not yet implemented");
+	}
+
+}
